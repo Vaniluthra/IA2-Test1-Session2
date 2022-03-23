0eca48c5e212a13ee1f068cff5d4151f316c558d
vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2022-03-23 14:25:45 +0000

    h

--- p3final.c
+++ p3final.c
@@ -10,21 +10,22 @@ int input_number()
 
 int is_composite(int n)
 {
-  int i;
-  for(i=2;i<n/2;i++)
+  if()
+  int p=0;
+  for(int i=1;i<=n;i++)
   {
     if(n%i==0)
-    return 0;
-    else
-    return 1;
+    p+=1;
   }
+  return p;
 }
 
 void output(int n, int composite)
-{  if(composite==0)
-  printf(" not  composite");
+{
+  if(composite==2)
+  printf("%d is a prime number\n",n);
   else
-  printf("composite");
+  printf("%d is a composite number\n",n);
 }
 
 int main()
